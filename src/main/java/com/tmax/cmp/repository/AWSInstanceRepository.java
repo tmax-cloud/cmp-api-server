4f191511df3d179cc143ae86a12ad4d44e96807c
aldlfkahs <aldlfkahs@gmail.com>
2021-10-14 14:39:46 +0900

    [mod] QUERY annotation 수정

--- src/main/java/com/tmax/cmp/repository/AWSInstanceRepository.java
+++ src/main/java/com/tmax/cmp/repository/AWSInstanceRepository.java
@@ -2,12 +2,16 @@ package com.tmax.cmp.repository;
 
 import com.tmax.cmp.dto.AWSInstanceDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AWSInstanceRepository extends JpaRepository<AWSInstanceDTO, Long>{
 
-    @Query(value = "delete * from awsinstance where not instance_id in (:exist_instances)", nativeQuery = true)
+    @Transactional
+    @Modifying
+    @Query("delete from AWSInstanceDTO where not instanceId in (:exist_instances)")
     void deleteExcept(@Param("exist_instances") String exist_instances);
 
 }
