19b5e1ff6b6e10c42e943480d0fb7c5f6f20bc41
2smin <planner263@gmail.com>
2022-05-25 17:06:55 +0900

    [hotfix] rollback awsClient regionList

--- src/main/java/com/tmax/cmp/entity/common/client/awsClient.java
+++ src/main/java/com/tmax/cmp/entity/common/client/awsClient.java
@@ -8,11 +8,16 @@ import software.amazon.awssdk.services.costexplorer.CostExplorerClient;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class awsClient implements Client{
 
-    private static final List<Region> REGIONS = List.of(Region.AP_SOUTH_1, Region.EU_SOUTH_1, Region.CA_CENTRAL_1, Region.EU_CENTRAL_1, Region.US_WEST_1, Region.US_WEST_2, Region.EU_NORTH_1, Region.EU_WEST_3, Region.EU_WEST_2, Region.EU_WEST_1, Region.AP_NORTHEAST_3, Region.AP_NORTHEAST_2, Region.AP_NORTHEAST_1, Region.SA_EAST_1, Region.AP_EAST_1, Region.CN_NORTH_1, Region.AP_SOUTHEAST_1, Region.AP_SOUTHEAST_2, Region.US_EAST_1, Region.US_EAST_2);
+    private static final List<Region> REGIONS = Collections.unmodifiableList(Arrays.asList(Region.AP_SOUTH_1, Region.EU_SOUTH_1,
+            Region.CA_CENTRAL_1, Region.EU_CENTRAL_1, Region.US_WEST_1, Region.US_WEST_2, Region.EU_NORTH_1, Region.EU_WEST_3, Region.EU_WEST_2,
+            Region.EU_WEST_1, Region.AP_NORTHEAST_3, Region.AP_NORTHEAST_2, Region.AP_NORTHEAST_1, Region.SA_EAST_1, Region.AP_EAST_1, Region.CN_NORTH_1,
+            Region.AP_SOUTHEAST_1, Region.AP_SOUTHEAST_2, Region.US_EAST_1, Region.US_EAST_2));
     private Ec2Client ec2Client;
     private CostExplorerClient costClient;
     @Getter
