14743bfd80edebb176d1e6f4529d78ac5bf3512a
2smin <planner263@gmail.com>
2022-05-25 15:55:23 +0900

    [modify] update clientConfig, credential svc,repo

--- src/main/java/com/tmax/cmp/repository/CredentialRepository.java
+++ src/main/java/com/tmax/cmp/repository/CredentialRepository.java
@@ -2,10 +2,7 @@ package com.tmax.cmp.repository;
 
 import com.tmax.cmp.entity.common.account.AwsCredentials;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CredentialRepository extends JpaRepository<AwsCredentials, Long> {
-
 
 }
