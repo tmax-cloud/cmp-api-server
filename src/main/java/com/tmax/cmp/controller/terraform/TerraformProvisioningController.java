8c670178ac04ed4adaea1b70c28e242ccbce1d99
zmrdltl <meenseek5929@naver.com>
2022-07-07 13:59:56 +0900

    무한루프 수행하는 자식 프로세스 예시 및 timeout error exception 추가

--- src/main/java/com/tmax/cmp/controller/terraform/TerraformProvisioningController.java
+++ src/main/java/com/tmax/cmp/controller/terraform/TerraformProvisioningController.java
@@ -1,6 +1,7 @@
 package com.tmax.cmp.controller.terraform;
 
 import com.tmax.cmp.svc.terraform.TerraformProvisioningService;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@EnableAsync
 @RestController
 @RequestMapping("/api/v1/terraform/resource")
 public class TerraformProvisioningController {
     TerraformProvisioningService terraformProvisioningService = new TerraformProvisioningService();
 
     @GetMapping("/create")
-    public void createInfraResource(@RequestParam String workingDirPath) throws IOException, InterruptedException {
+    public void createInfraResource(@RequestParam String workingDirPath) throws IOException, InterruptedException, NoSuchFieldException {
         terraformProvisioningService.createResource(workingDirPath);
     }
 
